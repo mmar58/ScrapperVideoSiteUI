4c6bc04f2d889b1617f44555d990413dd8b2fb2e
mmar58 <rahmanapu118@gmail.com>
2025-11-21 14:27:57 +0600

    Added current url

--- src/lib/stores/videoDb.ts
+++ src/lib/stores/videoDb.ts
@@ -2,3 +2,4 @@ import type { MediaDbEntry } from "$lib/types/mediaTypes";
 import { writable } from "svelte/store";
 
 export const videoDb = writable<MediaDbEntry[]>([]);
+export const currentViewingUrl = writable<string>("");
